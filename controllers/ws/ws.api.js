81df4b354b07ae19352c00d217b672aa53524f9d
Luis.O.M <luismunoz.dh@gmail.com>
2017-04-18 18:04:48 -0300

    sketchList event

--- controllers/ws/ws.api.js
+++ controllers/ws/ws.api.js
@@ -16,11 +16,19 @@ module.exports = function(httpServer,log){
                         {id:"1", name:"clip1", duration:"2000"},
                         {id:"2", name:"clip2", duration:"2500"},
                         ];
+            var sketchLst = [
+                        {id:"1", name:"sketch1", htmlContent:"<h1 style='color: blue;'>Gato</h1>"},
+                        {id:"2", name:"sketch2", htmlContent: "<h1 style='color: red;'>Atrevido</h1>"},
+                        {id:"3", name:"sketch3", htmlContent: "<h2 style='background-color:black; color:yellow;'>Te cabió</h2>"}
+                        ];
+            
 
-            socket.broadcast.emit("mediaListAllData", mediaLst);
+            socket.broadcast.emit("mediaList", mediaLst);
+            socket.broadcast.emit("sketchList", sketchLst);
 
         });
 
     });
 
 }
+
